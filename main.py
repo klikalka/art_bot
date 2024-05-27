d4ae35935234299567973b4947d186454da548ad
Cybersport <Cybersport@26kadr.internal>
2024-05-27 15:31:49 +0300

    trash

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 from config import TELEGRAM_TOKEN
 from random import randint
 from aiogram import Bot, Dispatcher, types, executor
-
+from neiro import get_response
 
 bot = Bot(token=TELEGRAM_TOKEN)
 dp = Dispatcher(bot)
@@ -45,7 +45,6 @@ def generate_image(prompt_text):
     while True:
         operation_response = requests.get(operation_url, headers=headers)
         operation_result = operation_response.json()
-        print(operation_result)
         if 'response' in operation_result:
             image_base64 = operation_result['response']['image']
             image_data = base64.b64decode(image_base64)
@@ -54,12 +53,13 @@ def generate_image(prompt_text):
             time.sleep(5)
 
 @dp.message_handler()
-async def handle_message(message: types.Message):
-    user_text = message.text
+async def analize_message(message: types.Message):
+    response_text = await get_response(message.text)
+    print(response_text)
     await message.reply('Идет генерация изображения, подождите пожавуста')
     try:
-        image_data = generate_image(user_text)
-        await message.reply_photo(photo= image_data)
+        image_data = generate_image(response_text)
+        await message.reply_photo(photo=image_data)
     except Exception as e:
         await message.reply(f'Произошла ошибка {e}')
 
